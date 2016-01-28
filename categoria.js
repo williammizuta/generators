5553fde15f13f21e60cee66e8f615f5e13abe73c
William Seiti Mizuta <william.mizuta@gmail.com>
2016-01-28 02:28:30 -0200

    Extracting method

--- categoria.js
+++ categoria.js
@@ -1,15 +1,15 @@
 define(['jquery'], function($) {
-  $.get('http://www.elo7.com.br/categoria/acessorios?sortBy=4&pageNum=1', function(data) {
-    $(data).find('.product').appendTo($('#products'));
-  });
-
-  var page = 2;
-  $('.category').on('click', function(e) {
+  var getPage = function(page) {
     $.get('http://www.elo7.com.br/categoria/acessorios?sortBy=4&pageNum=' + page, function(data) {
       $(data).find('.product').appendTo($('#products'));
     });
-    page++;
+  };
 
+  var page = 1;
+  getPage(page++);
+
+  $('.category').on('click', function(e) {
+    getPage(page++);
     e.preventDefault();
   });
 });
