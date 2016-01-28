219b6a325d44ba0cd90e8f36b8ee394034121de7
William Seiti Mizuta <william.mizuta@gmail.com>
2016-01-28 02:17:39 -0200

    Gets initial data

--- categoria.js
+++ categoria.js
@@ -1,2 +1,5 @@
 define(['jquery'], function($) {
+  $.get('http://www.elo7.com.br/categoria/acessorios?sortBy=4&pageNum=1', function(data) {
+    $(data).find('.product').appendTo($('#products'));
+  });
 });
